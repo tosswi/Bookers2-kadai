dcd38e27c5092c8d9502c3bce96b7be15617efe4
tosswi <Heckel3437@gmail.com>
2020-03-16 19:09:47 +0900

    Bookers2-kadai

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,50 @@
+class UsersController < ApplicationController
+	before_action :authenticate_user!
+  before_action :correct_user,   only: [:edit, :update]
+
+  def top
+  	if current_user
+        redirect_to user_path(current_user)
+    end
+  end
+
+  def index
+		@users=User.all
+    @book=Book.new
+    @user=current_user
+
+  end
+  def show
+  	@user=User.find(params[:id])
+  	@book1=User.find(params[:id]).books
+  	 @book=Book.new
+ 
+  end
+  def edit
+  	@user = User.find(params[:id])
+  end
+  def update
+  		@user = User.find(params[:id])
+  	if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+  	redirect_to user_path(@user.id)
+    else
+  	render :edit
+    end
+  end
+    def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to users_path
+  end
+private
+  def user_params
+      params.require(:user).permit(:name,:introduction,:profile_image)
+    end
+def correct_user
+  @user=User.find(params[:id])
+
+     redirect_to user_path(current_user) unless @user == current_user
+
+end
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'b500cf60ff9699c2f24454cb6f731655ed1d6e38553ec20c81a9e6ce400db0617e0b6665038c6d04c8f40345746a3f51696ad557f0d54f43a74786cb10ec00e1'
